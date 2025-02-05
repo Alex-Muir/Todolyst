df9032af6209dfa2bfa6f087f0ce3c66f1dba00d
Alex Muir <wamuir1212@protonmail.com>
2025-02-04 18:59:41 -0800

    removed item.py and to_do_lyst.py. changed code to use simple data structures

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import argparse
-
-from to_do_lyst import TodoList
+import json
 
 # A simple command line to-do list using argparse
 
+filename = 'todo_list.txt'
 
-my_todo_list = TodoList()
-my_todo_list.add_item("Make a bunch of money")
-my_todo_list.print_list()
+try:
+    with open(filename, encoding='utf-8') as f:
+        todo_list = json.load(f)
+except FileNotFoundError:
+    todo_list = []
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--add", help="add an item to your to-do list")
+parser.add_argument("-p", "--print", help="print your todo list", action="store_true")
+args = parser.parse_args()
+
+if args.add:
+    item = {
+        'position': len(todo_list) + 1,
+        'description': args.add,
+        'status': 'Incomplete'
+        }
+    todo_list.append(item)
+
+    try:
+        with open(filename, 'w') as f:
+            json.dump(todo_list, f)
+    except FileNotFoundError:
+        pass
+
+
+if args.print:
+    for item in todo_list:
+        for k, v, in item.items():
+            print(f"\n{k}: {v}")
+    
+    
 
 
