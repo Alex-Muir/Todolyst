1af8cf7d5e90e6b5dd74ef3ff8c43721c6e72a13
Alex Muir <wamuir1212@protonmail.com>
2025-02-06 16:52:00 -0800

    Refactored code and removed file_funcs.py

--- helpers.py
+++ helpers.py
@@ -1,20 +1,80 @@
+import argparse
+import json
+
 # helper functions for todo list
 
+
+# BASIC HELPERS
 def get_selection():
     """Get the user's selection for marking an item complete or for removing an item"""
     selection = input("Which item would you like to mark as complete?"  
                       " Enter a number or 'c' to cancel: ")
     return selection
 
-
-def reorder(index, todo_list):
-    """reorder the items 'position' on the list after an item removal"""
-    position = index + 1
-    for i in range(index, len(todo_list)):
-        todo_list[i]['position'] = position
-        position += 1
         
 def print_list(todo_list):
     """Print the todo list"""                                                      
-    for item in todo_list:                                                      
-        print(f"{item['position']}. {item['description']}\t\t{item['status']}")
+    for i in range(len(todo_list)):                                                      
+        print(f"{i+1} {todo_list[i]['description']}\t\t{todo_list[i]['status']}")
+
+# FILE HELPERS
+def save(filename, todo_list, message='your list has been saved'):
+    """Write the todo list to a file"""               
+    try:                                                                        
+        with open(filename, 'w') as f:                                          
+            json.dump(todo_list, f)                                             
+    except FileNotFoundError:                                                   
+        print("There seems to be an error with 'todo_list.txt'")                
+    else:                                                                       
+        print(message) 
+
+
+def load(filename):
+    """
+    Read the todo list from a file, if the file exists. If it doesn't exist
+    assume the program has not been run, initialize the todo list, and write
+    it to a file
+    """                                                           
+    try:                                                                        
+        with open(filename, encoding='utf-8') as f:                             
+            todo_list = json.load(f)                                            
+    except FileNotFoundError:                                                   
+        todo_list = []                                                          
+        write_to_file(todo_list, '')                                            
+    return todo_list  
+
+
+# TODO_LIST HELPERS
+def toggle_status(index, todo_list):
+    """Toggle the completion status of an item in the list"""
+    if todo_list[index]['status'] == 'Incomplete':
+        todo_list[index]['status'] = 'Complete'
+    else:
+        todo_list[index]['status'] = 'Incomplete'
+
+
+def completed_to_bottom(todo_list):
+    """Move the completed items to the bottom of the list"""
+    incomplete = [item for item in todo_list if item['status'].lower() == 'incomplete']
+    complete = [item for item in todo_list if item['status'].lower() == 'complete']
+    todo_list[:] = incomplete + complete
+    
+
+# PARSER HELPERS
+def set_up_parser(parser):
+    """Add arguments to the parser"""
+    parser.add_argument("-a", "--add", help="add an item to your to-do list")
+    parser.add_argument("-p", "--print", help="print your todo list", 
+                        action="store_true")
+    parser.add_argument("-c", "--clear", help="clear your todo list", 
+                        action="store_true")
+    parser.add_argument("-t", "--toggle", help="toggle item status",
+                        action="store_true")
+    parser.add_argument("-r", "--remove", help="remove an item from the todo list",
+                        action="store_true")
+    parser.add_argument("-m", "--move_to_bottom", 
+                        help="move completed items to the bottom of the list",
+                        action="store_true")
+    parser.add_argument("-C", "--clear_complete", 
+                        help="remove completed items from the todo_list", 
+                        action="store_true")
--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 import argparse
 
-import file_funcs as ff
 import helpers as h
 
-# A simple command line to-do list using argparse
+# A simple command line to-do list using argparse written by Alex Muir
     
 filename = 'todo_list.txt'
-todo_list = ff.read_from_file(filename)
+todo_list = h.load(filename)
 
 # set up parser and arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--add", help="add an item to your to-do list")
-parser.add_argument("-p", "--print", help="print your todo list", 
-                    action="store_true")
-parser.add_argument("-c", "--clear", help="clear your todo list", 
-                    action="store_true")
-parser.add_argument("-m", "--complete", help="mark an item complete",
-                    action="store_true")
-parser.add_argument("-r", "--remove", help="remove an item from the todo list",
-                    action="store_true")
-parser.add_argument("-b", "--move_to_bottom", 
-                    help="move completed items to the bottom of the list",
-                    action="store_true")
+h.set_up_parser(parser)
 args = parser.parse_args()
 
 # add a new item to the list
 if args.add:
     item = {
-        'position': len(todo_list) + 1,
         'description': args.add,
         'status': 'Incomplete'
         }
     todo_list.append(item)
-    ff.write_to_file(filename, todo_list)
+    h.save(filename, todo_list)
 
 # print the list
 if args.print:
@@ -45,15 +32,14 @@ if args.print:
 if args.clear:
     todo_list.clear()
     message = 'your list has been cleared'
-    ff.write_to_file(filename, todo_list, message)
+    h.save(filename, todo_list, message)
 
 # mark an item complete
-if args.complete:
+if args.toggle:
     if todo_list:
         h.print_list(todo_list)
         while True:
             selection = h.get_selection()
-
             if selection.lower() == 'c':
                 break
             else:
@@ -65,10 +51,9 @@ if args.complete:
                     if selection < 1 or selection > len(todo_list):
                         print("Invalid input. Enter a valid postion number")
                     else:
-                        index = selection - 1
-                        todo_list[index]['status'] = 'Complete'
+                        h.toggle_status(selection-1, todo_list)
                         message = "item has been marked as 'complete'"
-                        ff.write_to_file(filename, todo_list, message)
+                        h.save(filename, todo_list, message)
                         break
     else: 
         print("The list is empty")
@@ -93,19 +78,23 @@ if args.remove:
                     else:
                         index = selection - 1
                         todo_list.pop(index)
-                        h.reorder(index, todo_list)
                         message = "Item has been removed"
-                        ff.write_to_file(filename, todo_list, message)
+                        h.save(filename, todo_list, message)
                         break
+    else:
+        print("The list is empty")
     
 if args.move_to_bottom:
     if todo_list:
-        for i in range(len(todo_list)):
-            if todo_list[i]['status'].lower() == 'complete':
-                tmp = todo_list.pop(i)
-                todo_list.append(tmp)
-        ff.write_to_file(filename, todo_list)
+        h.completed_to_bottom(todo_list)
+        h.save(filename, todo_list)
     else:
         print("The list is empty")
 
 
+if args.clear_complete:
+    if todo_list:
+        todo_list = [item for item in todo_list if item['status'] == 'Incomplete']
+        h.save(filename, todo_list)
+    else:
+        print("The list is empty")
